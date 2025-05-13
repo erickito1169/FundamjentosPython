c937ecda31a48c453b5a9cc48f33df9c016bb32c
ericksito1169 <erickesluutt06Qgmail.com>
2025-05-13 07:28:19 -0600

    ejercicio 1

---
+++ ejercicio01.py
@@ -0,0 +1,6 @@
+#################################################################################
+# Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+################################################################################
+
+name = input("Escribe un nombre: \n")
+print ("Holaap " + name)
