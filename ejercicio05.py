7679976e1f69d383bda8d1959b083a54f92d942a
ericksito1169 <erickesluutt06Qgmail.com>
2025-05-14 05:33:48 -0600

    ejercicio 05

---
+++ ejercicio05.py
@@ -0,0 +1,27 @@
+################################################################################
+#Escribir un programa que convierta un valor dado en grados Fahrenheit a grados 
+# Celsius.
+################################################################################
+#Análisis
+#Tenemos que leer una temperatura en grados Fahrenheit y devolverla en grados 
+#celsius.
+#Datos de entrada: grados Fahrenheit (real)
+#Información de salida: grado Celsius (real)
+#Variables: fahrenheit, celsius (Real).
+#################################################################################
+# Diseño
+# 1. Leer la temperatura en grados Fahrenheit
+# 2. Calcular los grados celsius (C = (F-32)*5/9)
+# 3. Mostrar grados celsius
+#################################################################################
+
+
+fahrenheit = float(input("Escribe grados Fahrenheit: "))
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+#100°F equivalen a 36°C
+#formoat strings
+#f"{}"
+
+print(f"{ fahrenheit }°F equivalen a { celsius }°C")
