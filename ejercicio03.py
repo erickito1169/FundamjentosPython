8af9a25cb715a807e9aad8ccca2b2cfedc0a04bc
ericksito1169 <erickesluutt06Qgmail.com>
2025-05-14 04:49:40 -0600

    ejercicio 03

--- ejercicio03.py
+++ ejercicio03.py
@@ -4,10 +4,14 @@
 
 #hipotenusa ** 2 =cateto1 ** 2 + cateto2 ** 2
 
+## el operador doble ** permite elevar un numero a una potencia n
+## para operaciones matematicas avazadas importamos  la libreria math
+
+import math
+
 cateto1 = int(input("ingresa valor de cateto 1: "))
 cateto2 = int(input("ingresa valor de cateto 2: "))
 
-import math
 hipotenusa = math.sqrt(cateto1 ** 2 + cateto2 ** 2)
 
 print ("La hipoenusa del triangulo es: "+ str(hipotenusa))
