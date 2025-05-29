7981706c5cf8ec32e37f281383e171b402acb879
ericksito1169 <erickesluutt06Qgmail.com>
2025-05-29 02:53:47 -0600

    Ejercicio05 Repetitivas

---
+++ Repetitivas/ejercicio05.py
@@ -0,0 +1,14 @@
+#//################################################################################
+#//Algoritmo que pida caracteres e imprima 'VOCAL' si son vocales y 'NO VOCAL' 
+#//en caso contrario, el programa termina cuando se introduce un espacio.
+#//################################################################################
+
+while True:
+    letra = input("Ingresa una letra minuscula: ")
+    if letra == "a" or letra  == "e" or letra == "i" or letra == "o" or letra == "u":
+        print("Vocal") 
+    else:
+        if letra == " ":
+            break
+        print("No vocal")
+        
